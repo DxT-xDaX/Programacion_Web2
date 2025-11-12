398988e27c84ce942d2fb7996afed5b854f165c3
DxT-xDaX <axelyahirmrdez@gmail.com>
2025-11-12 01:16:47 -0600

    si

--- Uso de node/Servidor Express Html/app.js
+++ Uso de node/Servidor Express Html/app.js
@@ -3,11 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3002;
 
-// Servir archivos estáticos desde la carpeta actual
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  // Envía el archivo index.html
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
